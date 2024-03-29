ebc3d98700ab1a350f0462fc4c8542c318029f73
Dominic Willi <dominic.willi@students.bfh.ch>
2024-03-29 19:51:18 +0100

    small update

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,12 +4,7 @@
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-        <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-          </q-avatar>
-          Title
-        </q-toolbar-title>
+        <q-toolbar-title> revival </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
