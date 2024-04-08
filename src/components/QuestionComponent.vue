5b41f5bce060591aeb22bcc04596458987e8cd49
Dominic Willi <dominic.willi@students.bfh.ch>
2024-04-08 10:55:26 +0200

    added vueuse and a progress bar

--- src/components/QuestionComponent.vue
+++ src/components/QuestionComponent.vue
@@ -1,20 +1,24 @@
 <template>
-  <div>
+  <div class="q-pa-sm">
     <q-card bordered flat class="bg-grey-2">
       <q-card-section>
-        <div class="text-h6">Frage 1</div>
+        <div class="text-h6">{{ props.questionTitle }}</div>
         <div class="text-subtitle2">
-          Wie oft waren Sie wegen der durch die Krebserkrankung verursachten
-          Symptome bei einem/r Hausarzt/-ärztin oder Allgemeinarzt/-ärztin,
-          bevor Sie eine:n Spezialisten/-in konsultiert haben?
+          {{ props.questionText }}
         </div>
       </q-card-section>
     </q-card>
-    <q-option-group v-model="group" :options="options" color="primary">
+    <q-option-group
+      v-model="group"
+      :options="props.answerOptions"
+      color="primary"
+    >
       <template v-slot:label="opt">
         <div class="q-py-sm">{{ opt.label }}</div></template
       >
     </q-option-group>
+
+    <q-btn @click="testing">test-Emit</q-btn>
   </div>
 </template>
 
@@ -22,31 +26,15 @@
 import { ref } from "vue";
 const group = ref("op1");
 
-const options = [
-  {
-    label: "Nie – Ich bin direkt zu einem/r Spezialisten/-in gegangen",
-    value: "answ1",
-  },
-  {
-    label:
-      "Nie – Ich hatte keine Symptome und der Krebs wurde im Rahmen einer Routine-/ Nachuntersuchung oder bei einer Vorsorge-untersuchung entdeckt",
-    value: "answ2",
-  },
-  {
-    label: "Ich war 1 oder 2 Mal bei einem/r Arzt/Ärztin",
-    value: "answ3",
-  },
-  {
-    label: "Ich war 3 oder 4 Mal bei einem/r Arzt/Ärztin",
-    value: "answ4",
-  },
-  {
-    label: "Ich war 5 oder mehr Mal bei einem/r Arzt/ Ärztin",
-    value: "answ5",
-  },
-  {
-    label: "Ich weiss es nicht / Ich erinnere mich nicht",
-    value: "answ6",
-  },
-];
+const props = defineProps({
+  questionTitle: String,
+  questionText: String,
+  answerOptions: Array,
+});
+
+const options = props.answerOptions;
+const emit = defineEmits(["answerSelected"]);
+function testing() {
+  emit("answerSelected");
+}
 </script>
--- src/pages/pageOne.vue
+++ src/pages/pageOne.vue
@@ -1,10 +1,104 @@
 <template>
   <q-page>
-    <div>{{ $t("welcome") }}</div>
-    <QuestionComponent></QuestionComponent>
+    <q-linear-progress :value="count" class="q-mt-md" />
+    <QuestionComponent
+      :question-text="question.questionText"
+      :question-title="question.questionTitle"
+      :answer-options="question.answerOptions"
+      @answerSelected="print"
+    ></QuestionComponent>
+    <q-btn @click="previousPage">Zurück</q-btn>
+    <q-btn @click="nextPage">Weiter</q-btn>
   </q-page>
 </template>
 
 <script setup>
 import QuestionComponent from "../components/QuestionComponent.vue";
+import { useCounter } from "@vueuse/core";
+const { count, inc, dec, set, reset } = useCounter();
+
+function print() {
+  console.log("yeeeeting the emit");
+}
+
+function nextPage() {
+  inc(step);
+  console.log(count);
+}
+function previousPage() {
+  dec(step);
+}
+
+const question = {
+  questionTitle: "Frage 1",
+  questionText: "Wie geht es Ihnen?",
+  answerOptions: [
+    {
+      label: "Gut",
+      value: "answ1",
+    },
+    {
+      label: "Ok",
+      value: "answ2",
+    },
+    {
+      label: "Schlecht",
+      value: "answ3",
+    },
+  ],
+};
+
+const question2 = {
+  questionTitle: "Frage 2",
+  questionText: "Was ist Ihre Lieblingsfarbe?",
+  answerOptions: [
+    {
+      label: "Blau",
+      value: "answ1",
+    },
+    {
+      label: "Rot",
+      value: "answ2",
+    },
+    {
+      label: "Grün",
+      value: "answ3",
+    },
+    {
+      label: "Gelb",
+      value: "answ4",
+    },
+  ],
+};
+
+const question3 = {
+  questionTitle: "Frage 3",
+  questionText: "Welches Tier mögen Sie am meisten?",
+  answerOptions: [
+    {
+      label: "Hund",
+      value: "answ1",
+    },
+    {
+      label: "Katze",
+      value: "answ2",
+    },
+    {
+      label: "Vogel",
+      value: "answ3",
+    },
+    {
+      label: "Fisch",
+      value: "answ4",
+    },
+    {
+      label: "Keines",
+      value: "answ5",
+    },
+  ],
+};
+
+const questions = [question, question2, question3];
+
+const step = 1 / questions.length;
 </script>
