bdc8444595f210b92705a30bb75f0b2daf8572ee
Dominic Willi <dominic.willi@students.bfh.ch>
2024-04-11 11:53:52 +0200

    made some small chagnes

--- src/pages/questionnairePage.vue
+++ src/pages/questionnairePage.vue
@@ -1,24 +1,32 @@
 <template>
-  <FQRenderer
-    :key="$i18n.locale"
-    :questionnaire-data="qData"
-    :language="$i18n.locale.split('-')[0]"
-    :translation-strings="{
-      /* TranslationStrings object */
-    }"
-    @save="save"
-    @reset="reset"
-  />
+  <q-page>
+    <q-card>
+      <q-card-section>
+        <div class="text-body2 text-weight-medium text-justify q-py-sm">
+          Titel der Frage
+        </div>
+        <q-select label="Standard" outlined dense />
+      </q-card-section>
+    </q-card>
+
+    <FQRenderer
+      :key="$i18n.locale"
+      :questionnaire-data="qData"
+      :language="$i18n.locale.split('-')[0]"
+      :translation-strings="{
+        /* TranslationStrings object */
+      }"
+    />
+  </q-page>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import { FQRenderer } from "@i4mi/fhir-questionnaire-renderer";
 import { QuestionnaireData } from "@i4mi/fhir_questionnaire";
-
+import { FQRenderer } from "@i4mi/fhir-questionnaire-renderer";
 const url = "../jsonFiles/scape-copy.json";
 const data = ref("");
-const qData = ref(new QuestionnaireData("", ["de", "fr"]));
+const qData = ref(new QuestionnaireData("", ""));
 
 async function fetchData() {
   try {
@@ -27,19 +35,12 @@ async function fetchData() {
       throw new Error("Network response was not ok");
     }
     data.value = await response.json();
-    qData.value = new QuestionnaireData(data.value, ["de", "fr"]);
+    qData.value = new QuestionnaireData(data.value, ["de-CH", "fr-CH"]);
     console.log("qdata", qData.value);
+    console.log("Questions: ", qData.value.getQuestions());
   } catch (error) {
     console.error("Error fetching JSON:", error);
   }
 }
 fetchData();
-
-function save() {
-  // SEE to @i4mi/fhir_questionnaire documentation.
-}
-
-function reset() {
-  // SEE to @i4mi/fhir_questionnaire documentation.
-}
 </script>
