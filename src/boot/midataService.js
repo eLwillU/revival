98ae10894dc39eed656c8cb74f59e27fe50dbd18
Dominic Willi <dominic.willi@students.bfh.ch>
2024-04-22 11:09:41 +0200

    update

--- src/boot/midataService.js
+++ src/boot/midataService.js
@@ -1,14 +1,9 @@
 import { JSOnFhir } from "@i4mi/js-on-fhir";
 
-const fhir = new JSOnFhir(
-  "https://test.midata.coop",
-  "revival",
-  "http://localhost:9000/#/welcome",
-  {
-    disablePkce: false,
-    doesNotNeedAuth: false,
-  }
-);
+const fhir = new JSOnFhir("revival", "http://localhost:9000/#/welcome", {
+  disablePkce: false,
+  doesNotNeedAuth: false,
+});
 
 export default ({ app }) => {
   app.config.globalProperties.$fhir = fhir;
