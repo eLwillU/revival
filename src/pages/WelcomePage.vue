165b9636a58937fd4dfdba062a95bcd3f0772258
Dominic Willi <dominic.willi@students.bfh.ch>
2024-05-04 11:17:46 +0200

    changed plot

--- src/pages/WelcomePage.vue
+++ src/pages/WelcomePage.vue
@@ -17,9 +17,9 @@
         </q-card-actions>
       </q-card>
     </div>
-    <!---
+    <!--
     <div id="tester" style="width: 600px; height: 250px"></div>
-    -->
+  -->
   </q-page>
 </template>
 <script setup>
@@ -31,18 +31,18 @@ const store = userStore();
 // import { onMounted } from "vue";
 
 // onMounted(() => {
-//   var TESTER = document.getElementById("tester");
-//   Plotly.newPlot(
-//     TESTER,
-//     [
-//       {
-//         x: [1, 2, 3, 4, 5],
-//         y: [1, 2, 4, 8, 16],
-//       },
-//     ],
-//     {
-//       margin: { t: 0 },
-//     }
-//   );
-// });
+//  var TESTER = document.getElementById("tester");
+//  Plotly.newPlot(
+//    TESTER,
+//    [
+//      {
+//        x: [1, 2, 3, 4, 5],
+//        y: [1, 2, 4, 8, 16],
+//      },
+//    ],
+//    {
+//      margin: { t: 0 },
+//    }
+//  );
+//});
 </script>
