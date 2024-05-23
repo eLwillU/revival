404e83729adea76684863e88013930dd52bec563
Dominic Willi <dominic.willi@students.bfh.ch>
2024-05-23 13:53:29 +0200

    changed looks of the type page a little bit

--- src/pages/QuestionTypePage.vue
+++ src/pages/QuestionTypePage.vue
@@ -1,53 +1,52 @@
 <template>
   <div v-if="dataReady">
-    <div v-for="q in qData.getQuestions()" :key="q.id">
-      <div v-if="q.type === 'choice'">
-        <h6>
-          {{ q.label["de"] }}
-        </h6>
-        <ul>
-          <li
-            v-for="answer in q.answerOptions"
-            :key="q.id + '-' + answer.code"
-            @click="updateQuestionAnswers(q, answer)"
-          >
-            <input
-              :type="q.allowsMultipleAnswers ? 'checkbox' : 'radio'"
-              :checked="isAnswerOptionSelected(q, answer)"
-              :name="q.id"
-              :id="answer.code.toString()"
-            />
-            <label for="answer.code.toString()">
-              {{ answer.answer["de"] }}
-            </label>
-            {{ answer }}
-          </li>
-          <div v-for="qSub in q.subItems" :key="qSub.id">
-            <div v-if="qSub.isEnabled">
-              <h6>
-                {{ qSub.label["de"] }}
-              </h6>
-              <input type="text" v-model="singleAnswer" />
-              <button @click="send(qSub, singleAnswer)">Submit</button>
-              <button
-                @click="console.log(qData.getQuestionnaireResponse('de'))"
-              >
-                Log response
-              </button>
+    <q-card>
+      <q-card-section>
+        <div v-for="q in qData.getQuestions()" :key="q.id">
+          <div v-if="q.type === 'choice'">
+            <h6>
+              {{ q.label["de"] }}
+            </h6>
+            <div
+              v-for="answer in q.answerOptions"
+              :key="q.id + '-' + answer.code"
+            >
+              <q-radio
+                v-model="selectedAnswer"
+                :label="answer.answer['de']"
+                :val="answer.answer['de']"
+                @update:model-value="() => updateQuestionAnswers(q, answer)"
+              ></q-radio>
+            </div>
+            <div v-for="qSub in q.subItems" :key="qSub.id">
+              <div v-if="qSub.isEnabled">
+                <h6>
+                  {{ qSub.label["de"] }}
+                </h6>
+                <q-input v-model="singleAnswer" outlined> </q-input>
+                <button @click="send(qSub, singleAnswer)">Submit</button>
+                <button
+                  @click="console.log(qData.getQuestionnaireResponse('de'))"
+                >
+                  Log response
+                </button>
+              </div>
             </div>
           </div>
-        </ul>
-      </div>
-    </div>
+        </div>
+      </q-card-section>
+    </q-card>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 const qData = ref("");
+const selectedAnswer = ref("");
 const singleAnswer = ref("");
 import { QuestionnaireData } from "@i4mi/fhir_questionnaire";
 const dataReady = ref(false);
+
 fetch("questionnaire/type.json")
   .then((response) => response.json())
   .then((res) => {
@@ -60,10 +59,6 @@ fetch("questionnaire/type.json")
 
 function updateQuestionAnswers(q, answer) {
   qData.value.updateQuestionAnswers(q, answer);
-}
-
-function isAnswerOptionSelected(q, answer) {
-  return qData.value.isAnswerOptionSelected(q, answer);
 }
 
 function send(question, answer) {
