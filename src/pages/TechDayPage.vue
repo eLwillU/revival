f3ba327cc4ff9d7ca503e9a58fda7f3d6d8cbd59
Dominic Willi <dominic.willi@students.bfh.ch>
2024-06-09 21:40:27 +0200

    added questionnaire resource

--- src/pages/TechDayPage.vue
+++ src/pages/TechDayPage.vue
@@ -1,11 +1,17 @@
 <template>
   <q-page>
     <q-card class="q-mt-sm"
-      ><q-card-section
-        >Um den Feedback-Fragebogen auszufüllen, klicken sie auf den
-        Button</q-card-section
-      >
-      <q-card-actions><q-btn outline>Start</q-btn></q-card-actions>
+            primary
+    >
+      <q-card-section>
+        Um den Feedback-Fragebogen auszufüllen, klicken sie auf den
+        Button
+      </q-card-section>
+
+
+      <q-card-actions>
+        <q-btn outline>Start</q-btn>
+      </q-card-actions>
     </q-card>
   </q-page>
 </template>
