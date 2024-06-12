793959a6bab4a5e7b2b5d2b618d352c00065a831
Dominic Willi <dominic.willi@students.bfh.ch>
2024-06-12 22:23:52 +0200

    updated files

--- src/pages/TechDayPage.vue
+++ src/pages/TechDayPage.vue
@@ -1,13 +1,9 @@
 <template>
   <q-page>
-    <q-card class="q-mt-sm"
-            primary
-    >
+    <q-card class="q-mt-sm" primary>
       <q-card-section>
-        Um den Feedback-Fragebogen auszufüllen, klicken sie auf den
-        Button
+        Um den Feedback-Fragebogen auszufüllen, klicken sie auf den Button
       </q-card-section>
-
 
       <q-card-actions>
         <q-btn outline>Start</q-btn>
