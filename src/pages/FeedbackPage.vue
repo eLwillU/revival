9fba9114d5073090cb56eaf4de5baa3dd73c5dad
Dominic Willi <dominic.willi@students.bfh.ch>
2024-06-13 20:08:12 +0200

    build version for tech day tomorrow

--- src/pages/FeedbackPage.vue
+++ src/pages/FeedbackPage.vue
@@ -12,15 +12,11 @@ Currently its only available in german.
       </q-img>
       <q-card-section
         ><div>
-          Vielen Dank für Ihr Interesse! Sie haben nun die Möglichkeit entweder
-          unsere komplette Web-App mit dem originalen Fragebogen zu testen. Oder
-          Sie haben die Möglichkeit eine kurze Version eines Fragebogens
-          durchzuspielen, um ein Feedback zum Tech-Day zu geben.
+          Vielen Dank für Ihr Interesse! Sie haben nun die Möglichkeit unsere komplette Web-App mit dem originalen Fragebogen zu testen.
         </div>
       </q-card-section>
       <q-card-actions>
-        <q-btn color="primary">Ganze Web-App</q-btn>
-        <q-btn color="primary">Verkürzter Fragebogen</q-btn>
+        <q-btn to="/" color="primary">Hier klicken</q-btn>
       </q-card-actions>
     </q-card>
   </q-page>
